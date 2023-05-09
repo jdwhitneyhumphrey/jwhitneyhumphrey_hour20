b9a7da0c699eea5f2b4dfcd16098de8421e0d8a3
jdwhitneyhumphrey <124301860+jdwhitneyhumphrey@users.noreply.github.com>
2023-05-09 13:39:02 -0700

    Coding

--- Assets/Scenes/TextureScroller.cs
+++ Assets/Scenes/TextureScroller.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class TextureScroller : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public float speed = .5f;
+
+    Renderer renderer;
+    float offset;
+
     void Start()
     {
-        
+        renderer = GetComponent<Renderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Increase offset based on time
+        offset += Time.deltaTime * speed;
+        // Keep offset between 0 and 1
+        if (offset > 1)
+            offset -= 1;
+        //Apply the offset to the material
+        renderer.material.mainTextureOffset = new Vector2(0, offset);
     }
 }
