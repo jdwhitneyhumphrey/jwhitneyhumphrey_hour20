1d323920d23b8925aabbacc1cb0fdfcb9087bd54
jdwhitneyhumphrey <124301860+jdwhitneyhumphrey@users.noreply.github.com>
2023-05-11 11:19:06 -0700

    Currently frustrated...

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -2,61 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public TextureScroller ground;
-public float gameTime = 10;
-
-float totalTimeElapsed = 0;
-bool is GameOver = false;
-
-void Update()
+public class GameManager : MonoBehaviour
 {
-    if (isGameOver)
-        return;
+    public TextureScoller ground;
+    public float gameTime = 10;
 
-    totalTimeElapsed += totalTimeElapsed.deltaTime;
-    gameTime -= totalTimeElapsed.deltaTime;
+    float totalTimeElapsed = 0;
+    bool isGameOver = flase;
 
-    if (gameTime <= 0)
-        isGameOver = true;
-}
-
-public void AdjustTime(float amount)
-{
-    gameTime += amount;
-    if (amount < 0)
-        SlowWorldDown();
-}
-
-void SlowWorldDown()
-{
-    //Cancel any invokes to speed the world up
-    //Then slow the world down for 1 second
-    CancelInvoke();
-    Time.timeScale = 0.5f;
-    Invoke("SpeedWorldUp", 1);
-}
-
-void SpeedWorldUp()
-{
-    Time.timeScale = 1f;
-}
-//Note this is using Unity's legacy GUI system
-{
-    if(!isGameOver)
-  {
-        Rect boxRect = new Rect(Screenwidth / 2 - 50, Screen.height - 100, 100, 50);
-        GUI.Box(boxRect, "Time Remaining");
-
-        Rect labelRect = new boxRect(Screen.width / 2 - 10, Screen.height - 80, 20, 40);
-  }
-
-  else
-  {
-        Rect box Rect = new Rect(Screen.width / 2 - 60, Screen.height / 2 - 100, 120, 50);
-        GUI.Box(boxRect, "Game Over");
-        Rect labelRect = new Rect(Screen.width / 2 - 55, Screen.height / 2 - 80, 90, 40);
-        GUI.Label(labelRect, "Total Time: " + (int)totalTimeElasped);
-
-        Time.timeScale = 0;
-  }
+    void Update()
+    {
+        
+    }
 }
